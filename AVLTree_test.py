539ebc625dd3f808705778468b38c57a4cc78ae5
kb-math <kamil.bulinski@gmail.com>
2025-01-25 18:51:02 +1100

    first commit

---
+++ AVLTree_test.py
@@ -0,0 +1,48 @@
+from AVLTree import *
+
+def test_AVL_property(node):
+    assert(abs(calc_tree_height(node.left) - calc_tree_height(node.right)) < 2)
+    if node.left is not None:
+        test_AVL_property(node.left)
+    if node.right is not None:
+        test_AVL_property(node.right)
+
+def test_insert(node_keys_arr):
+    tree = AVLTree()
+    for key in node_keys_arr:
+        tree.insert_key(key)
+        test_AVL_property(tree.root)
+
+    assert(tree.to_sorted_array() == sorted(node_keys_arr))
+    print(plot_node(tree.root))
+    assert(calc_tree_height(tree.root) == tree.root.height)
+
+def test_delete(node_keys_arr = [0,1,2,3,4,5]):
+    tree = AVLTree()
+    for key in node_keys_arr:
+        tree.insert_key(key)
+
+    print(plot_node(tree.root))
+    for i in range(1,len(node_keys_arr)):
+        test_AVL_property(tree.root)
+        tree.delete(node_keys_arr[-i])
+        print(plot_node(tree.root))
+        assert(tree.to_sorted_array() == sorted(node_keys_arr[:-i]))
+
+def calc_tree_height(node):
+    if node is None:
+        return 0
+
+    height = 1
+    if node.left is not None:
+        height = max(height, calc_tree_height(node.left) + 1) 
+    if node.right is not None:
+        height = max(height, calc_tree_height(node.right) + 1)
+
+    return height
+
+def plot_node(node):
+    if node is None:
+        return ""
+
+    return "(" + plot_node(node.left) + " " + str(node.key) + " " + plot_node(node.right) + ")"
